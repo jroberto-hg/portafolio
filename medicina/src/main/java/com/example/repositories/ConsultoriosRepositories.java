780c3c3b3f0d5f0a0b2342b64f60cad7f9479d8b
jroberto-hg <jroberto.hg86@gmail.com>
2025-05-13 16:48:54 -0700

    Delete medicina directory

--- medicina/src/main/java/com/example/repositories/ConsultoriosRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import com.example.models.ConsultoriosModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultoriosRepositories extends JpaRepository<ConsultoriosModels, Long> {
-}
--- medicina/src/main/java/com/example/repositories/DoctorRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import com.example.models.DoctoresModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepositories extends JpaRepository<DoctoresModels, Long> {
-}
