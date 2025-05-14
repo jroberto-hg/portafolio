727f6a7a28af9e1615041687f0585efe95b0786e
jroberto-hg <jroberto.hg86@gmail.com>
2025-05-14 10:26:04 -0700

    Add files via upload

--- medicina/src/main/java/com/example/medicina/config/CorsConfig.java
+++ medicina/src/main/java/com/example/medicina/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.medicina.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings (CorsRegistry registry){
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins("*")
