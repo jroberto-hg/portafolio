43d18788977b5c8b1b66190170deeb731d9c7ec1
jroberto-hg <jroberto.hg86@gmail.com>
2025-05-14 09:26:36 -0700

    Add files via upload

--- medicina/src/main/java/com/example/medicina/controllers/CitasController.java
+++ medicina/src/main/java/com/example/medicina/controllers/CitasController.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/citas")
 public class CitasController {
     private final CitasServices citaServicio;
